95bf4495aa181ebcd3ff9fa6190d596c3d46be89
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-10 05:22:01 +0800

    JS: Added the same function as getTwelveCharacters but will loop until 16, 20, or 24 indices instead of 12

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ function getTwelveCharacters() {
     passwordEl.textContent = password;
 }
 
+function getSixteenCharacters() {
+    let password = "";
+    for (let i = 0; i < 16; i++) {
+        let generateNumber = getRandomNumber()
+        password += characters[generateNumber];
+    }
+    passwordEl.textContent = password;
+}
+
+function getTwentyCharacters() {
+    let password = "";
+    for (let i = 0; i < 20; i++) {
+        let generateNumber = getRandomNumber()
+        password += characters[generateNumber];
+    }
+    passwordEl.textContent = password;
+}
+
+function getTwentyFourCharacters() {
+    let password = "";
+    for (let i = 0; i < 24; i++) {
+        let generateNumber = getRandomNumber()
+        password += characters[generateNumber];
+    }
+    passwordEl.textContent = password;
+}
+
 function resetPassword() {
     let password = "Your password will be generated here...";
     passwordEl.textContent = password;
