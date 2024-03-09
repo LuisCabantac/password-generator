d4ca5a1b3602a8d29d0a7c082b1c51c73196aae5
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-10 03:28:09 +0800

    JS: Added a getRandomNumber function that will generate random number based on the length of the characters variable

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()_-{}[]|:;'"<.>,?`;
 let passwordEl = document.querySelector("#password-el");
+
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * characters.length);
+    return randomNumber;
+}
