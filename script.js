f5e6ef7280b7e70e7f65c4533fdd18fb57d8c3db
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-10 03:30:23 +0800

    JS: Added a generatePassword function that will generate random characters from the variable with same name and will run the getRandomNumber function in a loop and will render out the all 8 random characters

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function getRandomNumber() {
     let randomNumber = Math.floor(Math.random() * characters.length);
     return randomNumber;
 }
+
+function generatePassword() {
+    let password = "";
+    for (let i = 0; i < 8; i++) {
+        let generateNumber = getRandomNumber()
+        password += characters[generateNumber];
+    }
+    passwordEl.textContent = password;
+}
